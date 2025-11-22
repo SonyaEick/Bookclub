0fd198d2d4ca4b090b13341910496f9283088ba5
Sonya Eick <seick.wa@gmail.com>
2025-11-22 17:58:15 -0600

    Added Depends dependency injection; fixed unique constriant; fixed database locking issue

--- server/api/books.py
+++ server/api/books.py
@@ -1,5 +1,5 @@
 import random
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from db.session import get_db
 from models.book import books_table
 from .websocket import manager
@@ -7,42 +7,33 @@ from .websocket import manager
 router = APIRouter()
 
 @router.get("/")
-def get_books():
-    db = get_db()
+def get_books(db = Depends(get_db)):
     result = db.execute(books_table.select()).fetchall()
-    db.close()
     return [{"id": r[0], "title": r[1]} for r in result]
-    # return [{"id": r[0], "title": r[1], "added_by": r[2]} for r in result]
+
 
 @router.post("/add/")
-def add_book(title: str):
-# def add_book(title: str, added_by: str):
-    db = get_db()
+def add_book(title: str, db = Depends(get_db)):
     db.execute(books_table.insert().values(title=title))
-    # db.execute(books_table.insert().values(title=title, added_by:added_by))
     db.commit()
-    db.close()
     return {"message": f"Book '{title}' added!"}
 
+
 @router.post("/eliminate/")
-async def eliminate_random():
-    db = get_db()
+async def eliminate_random(db = Depends(get_db)):
     result = db.execute(books_table.select()).fetchall()
 
     if not result:
-        db.close()
         return {"message": "No books left"}
 
     if len(result) == 1:
         winner = result[0][1]
-        db.close()
         await manager.broadcast({"winner": winner})
         return {"message": "Winner chosen!", "winner": winner}
 
     chosen = random.choice(result)
     db.execute(books_table.delete().where(books_table.c.id == chosen[0]))
     db.commit()
-    db.close()
 
     await manager.broadcast({"eliminated": chosen[1]})
     return {"message": f"Eliminated {chosen[1]}"}
--- server/db/session.py
+++ server/db/session.py
@@ -8,4 +8,8 @@ metadata = MetaData()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
-    return SessionLocal()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
